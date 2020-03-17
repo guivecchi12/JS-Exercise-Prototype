8813193c58aff99e64ed56c8a550c666bf7ff975
unknown <guivecchi12@gmail.com>
2020-03-16 21:07:30 -0600

    21/27

--- index.js
+++ index.js
@@ -39,9 +39,22 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-  console.log("hi");
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+Person.prototype.eat = function(someFood){
+  if(this.stomach.length < 10){
+    this.stomach.push(someFood);
+  }
+}
+Person.prototype.poop = function(){
+  return this.stomach = [];
+};
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+};
 
 /*
   TASK 2
@@ -57,10 +70,30 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
-
+Car.prototype.fill = function(gallons){
+  return this.tank += gallons;
+};
+Car.prototype.drive = function(distance){
+  let maxD = this.tank * this.milesPerGallon;
+  if( maxD < distance){
+    this.odometer = maxD;
+  }
+  else{
+    this.odometer += distance;
+  }
+  
+  this.tank -= distance / this.milesPerGallon;
+  if(this.tank <= 0){
+    this.tank = 0;
+    return `I ran out of fuel at ${this.odometer} miles!`;
+  }
+};
 /*
   TASK 3
     - Write a Baby constructor subclassing Person.
